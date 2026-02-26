80f15aaecbc1da2d9daddb59c56c97c8241797b7
anonymous-shanks <shanks9628@gmail.com>
2026-02-26 11:26:27 +0530

    Update FoldersPreferences.kt

--- app/src/main/java/app/marlboroadvance/mpvex/preferences/FoldersPreferences.kt
+++ app/src/main/java/app/marlboroadvance/mpvex/preferences/FoldersPreferences.kt
@@ -11,6 +11,6 @@ class FoldersPreferences(
   // Set of folder paths that should be hidden from the folder list
   val blacklistedFolders = preferenceStore.getStringSet("blacklisted_folders", emptySet())
 
-  // Set of folder paths that are pinned to the top
-  val pinnedFolders = preferenceStore.getStringSet("pinned_folders", emptySet())
+  // Ordered string of pinned folder paths (separated by |) to preserve user arrangement
+  val pinnedFoldersOrder = preferenceStore.getString("pinned_folders_order", "")
 }
