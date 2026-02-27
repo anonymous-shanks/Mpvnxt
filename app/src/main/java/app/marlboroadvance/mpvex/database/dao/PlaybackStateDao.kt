1f51ca110075925d6ab79cc0383f5fe38ae66315
anonymous-shanks <shanks9628@gmail.com>
2026-02-27 14:02:56 +0530

    Update PlaybackStateDao.kt

--- app/src/main/java/app/marlboroadvance/mpvex/database/dao/PlaybackStateDao.kt
+++ app/src/main/java/app/marlboroadvance/mpvex/database/dao/PlaybackStateDao.kt
@@ -1,10 +1,12 @@
 package app.marlboroadvance.mpvex.database.dao
 
+import androidx.annotation.Keep
 import androidx.room.Dao
 import androidx.room.Query
 import androidx.room.Upsert
 import app.marlboroadvance.mpvex.database.entities.PlaybackStateEntity
 
+@Keep // <-- YEH R8 COMPILER KO CRASH KARNE SE ROKEGA
 @Dao
 interface PlaybackStateDao {
   @Upsert
